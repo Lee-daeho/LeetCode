346509048d2e173a5909e5e36b6a1e9a4c1a0b91
Lee-daeho <zmnh2003@gmail.com>
2022-07-28 00:41:20 +0900

    7/28 BFS will be added

---
+++ Easy/111_Minimum_Depth_of_Binary_Tree.py
@@ -0,0 +1,22 @@
+### 111. Minimum Depth of Binary Tree 22/07/28 ###
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+###My Solution of DFS after cheating (recursive)
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        if root.right and not root.left:
+            return self.minDepth(root.right) + 1
+
+        if root.left and not root.right:
+            return self.minDepth(root.left) + 1
+
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
