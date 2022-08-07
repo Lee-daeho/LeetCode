6557966cf0a2f92a65399cfe5d2f58336daa2a01
Lee-daeho <zmnh2003@gmail.com>
2022-08-07 22:50:24 +0900

    8/7

---
+++ Easy/100_Same_Tree.py
@@ -0,0 +1,19 @@
+### 100. Same Tree 22/08/07 ###
+
+###My Solution for recursive
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+        else:
+            return p == q
